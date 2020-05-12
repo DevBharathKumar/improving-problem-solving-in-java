8933cdb11f91251b5ab5b951de152b1d6278101a
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 16:27:35 +0530

    Printing the integers

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/PrintIntegers.java
@@ -0,0 +1,14 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class PrintIntegers 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Printing values from 1 to 10");
+		
+		for(int i=1 ; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
