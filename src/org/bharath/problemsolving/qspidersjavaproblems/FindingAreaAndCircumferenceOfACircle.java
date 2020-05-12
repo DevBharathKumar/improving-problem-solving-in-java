4759c149e9179de7f59a181b45d5acdee3f3cb72
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 16:14:36 +0530

    Finding area and circumference of a circle

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/FindingAreaAndCircumferenceOfACircle.java
@@ -0,0 +1,17 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class FindingAreaAndCircumferenceOfACircle {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int radius = 10;
+		double pie = 3.14;
+		
+		double area = pie*radius*radius;
+		double circum = 2*pie*radius;
+				
+		System.out.println("The area of the circle is "+area+" and the circumference of the circle is "+circum);
+	}
+
+}
