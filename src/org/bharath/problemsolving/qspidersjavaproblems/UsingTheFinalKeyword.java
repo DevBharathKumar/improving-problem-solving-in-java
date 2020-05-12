036e21e2a19fded8d267235e47d8e547172b14b8
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 14:24:17 +0530

    Using the final keyword

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/UsingTheFinalKeyword.java
@@ -0,0 +1,17 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class UsingTheFinalKeyword {
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("*************");
+		
+		final double pie = 3.14;
+		
+		System.out.println("Value is "+pie);
+		
+		//pie = 6.27; // Final value cannot be reassigned 
+		
+		System.out.println("*************");
+	}
+}
