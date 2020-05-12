8a630fa57829f94cd6e3a1d71104e96ca560932c
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 13:43:00 +0530

    Difference between println and print

---
+++ src/org/bharath/problemsolving/DifferenceBetweenPrintlnAndPrint.java
@@ -0,0 +1,13 @@
+package org.bharath.problemsolving;
+
+public class DifferenceBetweenPrintlnAndPrint {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Inside the println after printing the cursor will be moved to the next line");
+		System.out.print("Inside the print after printing the cursor will not be moved to the next line");
+		System.out.println(" *** CURSOR NOT MOVED *** ");
+
+	}
+
+}
