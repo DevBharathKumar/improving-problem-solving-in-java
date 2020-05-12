7c26942571c4dd4f5d7c7db1ceb238aa641b38fa
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 16:32:01 +0530

    Find odd or even

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/OddOrEven.java
@@ -0,0 +1,16 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class OddOrEven 
+{
+	public static void main(String[] args) {
+		
+		int value = 16;
+		
+		if(value%2==0) {
+			System.out.println("The value "+value+" is even");
+		}else {
+			System.out.println("The value "+value+" is odd");
+		}
+		
+	}
+}
