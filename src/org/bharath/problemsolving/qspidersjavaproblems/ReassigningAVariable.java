b5cd2cbb9fad863ee3e5b240f5e1dae9535ec12c
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 14:16:48 +0530

    Reassigning a variable

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/ReassigningAVariable.java
@@ -0,0 +1,16 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class ReassigningAVariable {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		System.out.println("Intail assignment of the value is "+a);
+		
+		a = 20;
+		
+		System.out.println("After reasigning the value is "+a);
+		
+	}
+}
