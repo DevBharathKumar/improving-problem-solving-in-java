de0e47239b27d1fa184df8aeeb801c8c88e69d3e
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 16:10:16 +0530

    Adding two numbers

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/AddingTwoNumbers.java
@@ -0,0 +1,12 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class AddingTwoNumbers {
+	
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("The sum of "+a+" and "+b+" is = "+c);
+	}
+}
