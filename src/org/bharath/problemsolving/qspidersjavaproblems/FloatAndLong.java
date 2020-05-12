31c57fd116c939ce9fd8111e913db4f453e57921
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 16:07:38 +0530

    Float and long value

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/FloatAndLong.java
@@ -0,0 +1,16 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class FloatAndLong 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("*************");
+		
+		float f = 12.78f;
+		long l = 9094605033l;
+		
+		System.out.println("Float value is "+f+" and the long value is "+l);
+		
+		System.out.println("*************");
+	}
+}
