ceab6487dcc49a43c0a8da66b18c94e4c90def3d
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 17:37:22 +0530

    Adding two numbers using method

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/AddingTwoNumbersUsingMethod.java
@@ -0,0 +1,16 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class AddingTwoNumbersUsingMethod {
+
+	
+	static int addTwoNumbers(int a,int b)
+	{
+		return a+b;
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(addTwoNumbers(4, 4));
+	}
+
+}
