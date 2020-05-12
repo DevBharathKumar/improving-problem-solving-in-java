d982e877318a679b83dddedf766e519cd9b5906c
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 13:58:41 +0530

    Understanding the java concatenation

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/UnderstandingTheJavaConcatenation.java
@@ -0,0 +1,18 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class UnderstandingTheJavaConcatenation 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("******** GUESS THE OUTPUTS ********");
+		System.out.println("10");
+		System.out.println('A');
+		System.out.println(20+20);
+		System.out.println("20"+"20");
+		System.out.println("The number is "+20);
+		System.out.println("The number is "+20+20);
+		System.out.println(20+20+" is the number ");
+		System.out.println("no = "+(20+20));
+		System.out.println("************************************");
+	}
+}
