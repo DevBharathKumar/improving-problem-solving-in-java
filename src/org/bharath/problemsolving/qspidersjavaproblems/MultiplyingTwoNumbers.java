36262cd130e00e3e4e650efb5a76613d4c05415d
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 17:29:12 +0530

    Multiplying two numbers

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/MultiplyingTwoNumbers.java
@@ -0,0 +1,12 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class MultiplyingTwoNumbers 
+{
+	public static void main(String[] args) 
+	{
+		double a = 6.7;
+		double b = 8.7;
+		double c = a*b;
+		System.out.println("When "+a+" is multiplied with "+b+" the output is "+c);
+	}
+}
