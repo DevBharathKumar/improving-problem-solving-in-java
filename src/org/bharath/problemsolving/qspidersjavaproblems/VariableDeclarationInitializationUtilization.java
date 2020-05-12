3d1629827f66f107d38027acc2abfd343880b2c3
Bharath Kumar <bharathk494@gmail.com>
2020-05-12 14:04:06 +0530

    Undestanding the variable declaration initialization and utilization

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/VariableDeclarationInitializationUtilization.java
@@ -0,0 +1,27 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class VariableDeclarationInitializationUtilization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Undestanding the variable declaration initialization and utilization");
+		
+		//Variable declaration
+		int id;
+		String name;
+		double marks;
+		
+		//Variable initialization
+		id = 1;
+		name = "Bharath Kumar";
+		marks = 78.8;
+		
+		//Variable utilization
+		System.out.println("Id is "+id);
+		System.out.println("Name is "+name);
+		System.out.println("Marks is "+marks);
+
+	}
+
+}
