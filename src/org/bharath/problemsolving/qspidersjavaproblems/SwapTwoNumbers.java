2914570b1ad36421627f059fb662cc6477070bd4
Bharath Kumar <bharathk494@gmail.com>
2020-05-14 11:57:10 +0530

    Swap two numbers

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class SwapTwoNumbers 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("*********** SWAPPING TWO NUMBERS ***********");
+		
+		int a = 10;
+		int b = 20;
+		System.out.println("Before swaping a is "+a+" b is "+b);
+		
+		int c = a;
+		a = b;
+		b = c;
+		
+		System.out.println("After swaping a is "+a+" b is "+b);
+		
+	}
+}
