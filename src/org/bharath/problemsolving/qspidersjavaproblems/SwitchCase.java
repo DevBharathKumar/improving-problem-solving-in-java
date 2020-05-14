1b10da24d0edd25ee089c215a4cd081f1539499b
Bharath Kumar <bharathk494@gmail.com>
2020-05-14 11:56:34 +0530

    Program to understand switch case

---
+++ src/org/bharath/problemsolving/qspidersjavaproblems/SwitchCase.java
@@ -0,0 +1,19 @@
+package org.bharath.problemsolving.qspidersjavaproblems;
+
+public class SwitchCase {
+
+	public static void main(String[] args) 
+	{
+		String s = "Rakesh";
+		
+		switch(s)
+		{
+		case "Bharath":System.out.println("I am inside bharath");
+		break;
+		case "Rakesh":System.out.println("I am inside rakesh");
+		break;
+		default:System.out.println("Inside default");
+		}
+	}
+
+}
